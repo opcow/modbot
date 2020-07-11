cb4b6a49ec48293c587d090a7cdada70a414b869
Mitch Crane <mitch.crane@gmail.com>
2020-07-11 16:04:54 -0400

    renamed calbot

--- build-plugins.sh
+++ build-plugins.sh
@@ -5,5 +5,5 @@ go build -buildmode=plugin ./covidbot/covidbot.go
 go build -buildmode=plugin ./reactbot/reactbot.go
 go build -buildmode=plugin ./pongbot/pongbot.go
 go build -buildmode=plugin ./saybot/saybot.go
-go build -buildmode=plugin ./calbot/calbot.go
+go build -buildmode=plugin ./bdbot/bdbot.go
 cd ..
